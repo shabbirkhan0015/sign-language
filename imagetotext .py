4bd96370f5a84ed3dcd59fd76bac096948f02307
shabbirkhan0015 <shabbirkhan0015@gmail.com>
2019-02-14 08:47:52 +0530

    first commit

---
+++ imagetotext .py
@@ -0,0 +1,6 @@
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+img = Image.open("image.png")## path of image
+text = pytesseract.image_to_string(img)
+print(text)
